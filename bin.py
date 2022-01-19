fccdd1cc1a5d10bc33f3e82924b4a8a99b5f346b
Abdoul Wahab Toure <45538965+abd2re@users.noreply.github.com>
2022-01-19 18:50:11 +0000

    Converts letters into binary numbers
    Converts letters into binary numbers with a length of 5

---
+++ bin.py
@@ -0,0 +1,28 @@
+char = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def conv(n):
+    a = ""
+    while n > 0:
+        a = a+str(n%2)
+        n = n // 2
+    r = 5 - len(a)
+    for i in range(r):
+        a = a + "0"
+    return a[::-1]
+
+
+def bin(arr, phrase):
+    phrase = phrase.lower()
+    binary = ""
+    for let in phrase:
+        for i in arr:
+            if let == i:
+                x = arr.index(i)
+                binary = binary+" "+conv(x)
+            else:
+                continue
+    print(binary)
+
+exem = input("Mettez votre phrase et je la transforme en ecriture binaire: ")
+bin(char,exem)
+
