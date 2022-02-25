b3cacc874c11e22de6cf7693ae7d737fad934b34
Abdoul Wahab Toure <45538965+abd2re@users.noreply.github.com>
2022-02-25 15:58:43 +0000

    Update arrowcounts.py

--- arrowcounts.py
+++ arrowcounts.py
@@ -1,5 +1,6 @@
-p = input("Input:")
+p = input('Inp :')
 c = 0
+d = 0
 h = []
 v = []
 
@@ -12,8 +13,13 @@ for i in h:
     if i == '>': c += 1
     elif i == '<': c -= 1
 
+for i in v:
+    if i == '^': d += 1
+    elif i == 'v': d -= 1
 
-print(len(h)-abs(c) + len(v))
+
+
+print(p,(len(h)-abs(c) + len(v)-abs(d)))
 
 #Given a string of arrows ^ > V and <, find the minimum number of turns you 
 #would need to make to point all the arrows in the same direction, eg. for ^^^>,
